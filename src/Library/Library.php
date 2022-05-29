746dbe4597c1e3b784e05849f92217c7c9a4bece
Westy90 <mawj15@student.bth.se>
2022-05-29 21:13:45 +0200

    Skapat en route för library

---
+++ src/Library/Library.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Library;
+
+class Library
+{
+    protected int $value;
+
+    public function __construct()
+    {
+        $this->value = random_int(1, 6);
+    }
+
+    public function roll(): int
+    {
+        $this->value = random_int(1, 6);
+        return $this->value;
+    }
+
+    public function getAsString(): string
+    {
+        return "[{$this->value}]";
+    }
+}
