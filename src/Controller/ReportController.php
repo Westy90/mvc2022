5cc3cbc12967ee54e36d77f7193aefc273513f74
Westy90 <mawj15@student.bth.se>
2022-06-09 21:46:33 +0200

    Påbörjat skriva rapporten CCCC

--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -25,6 +25,14 @@ class ReportController extends AbstractController
     }
 
     /**
+     * @Route("/metrics", name ="metrics")
+     */
+    public function metrics(): Response
+    {
+        return $this->render('metrics.html.twig');
+    }
+
+    /**
      * @Route("/about", name ="about")
      */
     public function about(): Response
